a6093cf6f990e0b76452bb704aa0fb946be95d3e
Aditya Sharma <adityasharma2289@gmail.com>
2026-05-05 18:03:42 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,13 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-pool
-  .connect()
-  .then(() => console.log("✅ PostgreSQL connected (Supabase)"))
+pool.connect()
+  .then(() => console.log("✅ PostgreSQL connected"))
   .catch((err) => console.error("❌ DB connection error:", err));
 
 module.exports = pool;
