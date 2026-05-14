1803672b4cc373e83cfbf3739ea76a91f61ba627
Aditya Sharma <adityasharma2289@gmail.com>
2026-05-14 17:06:56 +0530

    fix router context

--- chat-client/src/index.js
+++ chat-client/src/index.js
@@ -1,18 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "./ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App";
+
+import {
+  BrowserRouter,
+} from "react-router-dom";
+
+const root =
+  ReactDOM.createRoot(
+    document.getElementById("root")
+  );
 
 root.render(
+
   <React.StrictMode>
-    <ThemeProvider>
+
+    <BrowserRouter>
+
       <App />
-    </ThemeProvider>
+
+    </BrowserRouter>
+
   </React.StrictMode>
 );
-
-reportWebVitals();
